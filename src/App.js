00c62538e94b5561c34f59d186fd2694613fc474
swathi <swathisamudrala2004@gmail.com>
2024-03-10 21:15:28 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{useReducer} from 'react';
+import "./styles.css"
+const ACTIONS={
+  ADD_DIGIT:'add-digit',
+  CHOOSE_OPERATION:'choose-operation',
+  CLEAR:'clear',
+  DELETE_DIGIT:'delete-digit',
+  EVALUATE:'evaluate'
 }
-
+function reducer(state,{type,payload}){
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      return state
+  }
+}
+function App () {
+  const[{currentOperand,previousOperand,operation},dispatch]=useReducer(reducer,{})
+  return(
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previousOperand">{previousOperand} {operation}< /div>
+          <div className="current-operand">{currentOperand}</div>
+        </div>
+        <button className="span-two">AC</button>
+        <button>DEL</button>
+        <button>/</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button className="span-two">=</button>
+    </div>
+    
+  )
+}
 export default App;
